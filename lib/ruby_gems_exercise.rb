4acdc629e64e762545bf7dd682f196c3a18fbdd5
kiembully <kimklintonvale@gmail.com>
2024-01-24 10:40:20 +0800

    gems activity

--- lib/ruby_gems_exercise.rb
+++ lib/ruby_gems_exercise.rb
@@ -1,2 +1,25 @@
 class RubyGemsExercise
+  WORDS = ["lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit"]
+
+  def self.words(number, word_list = WORDS)
+    generate_text(number, word_list).join(' ')
+  end
+
+  def self.sentences(number, word_list = WORDS)
+    sentences = generate_text(number, word_list).join('. ')
+    sentences.split('. ').map(&:capitalize).join('. ')
+  end
+
+  def self.paragraphs(number, word_list = WORDS)
+    paragraphs = []
+    number.times { paragraphs << generate_text(rand(5..15), word_list).join(' ') }
+    paragraphs.join("\n")
+  end
+
+  private
+
+  def self.generate_text(number, word_list)
+    words = word_list * ((number / word_list.length) + 1)
+    words.sample(number)
+  end
 end
--- ruby_gems_exercise.gemspec
+++ ruby_gems_exercise.gemspec
@@ -4,6 +4,10 @@ Gem::Specification.new do |s|
   s.summary = "Gems exercise"
   s.description = "A simple placeholder text generator"
   # fill these fields in
-  s.authors = []
-  s.files = []
+  s.authors = ["Kim"]
+  s.files = [
+    "lib/ruby_gems_exercise.rb",
+    "spec/ruby_gems_exercise_spec.rb",
+    "ruby_gems_exercise.gemspec"
+  ]
 end
